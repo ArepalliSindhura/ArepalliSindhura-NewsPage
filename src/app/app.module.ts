04f471a3b385a0107402f36338f03471cb6b5184
ArepalliSindhura <17wh1a05a6@bvrithyderabad.edu.in>
2020-04-04 18:03:50 +0530

    committing changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FiltersBarComponent } from './filters-bar/filters-bar.component';
 import { FooterComponent } from './footer/footer.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { NewsLinesComponent } from './news-lines/news-lines.component';
-import { SourcenameBarComponent } from './sourcename-bar/sourcename-bar.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FiltersBarComponent,
     FooterComponent,
-    NavbarComponent,
     NewsLinesComponent,
-    SourcenameBarComponent
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
